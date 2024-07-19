7936e4adad27d0aaae430afbef472c1d6e314825
TroyMoses <mugabimoses07@gmail.com>
2024-07-20 01:45:17 +0800

    todo

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -8,9 +8,9 @@ class ToDo {
   static List<ToDo> todoList() {
     return [
       ToDo(id: '01', todoText: 'Morning Devotion', isDone: true),
-      ToDo(id: '02', todoText: 'Buy Breakfast', isDone: true),
+      ToDo(id: '02', todoText: 'Buy Breakfast', isDone: false),
       ToDo(id: '03', todoText: 'Make Breakfast', isDone: false),
-      ToDo(id: '04', todoText: 'Check Socials', isDone: true),
+      ToDo(id: '04', todoText: 'Check Socials', isDone: false),
       ToDo(id: '05', todoText: 'Buy Eggs', isDone: true),
       ToDo(id: '06', todoText: 'Buy Bread', isDone: false),
     ];
