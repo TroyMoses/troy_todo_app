7333ca92220b562f75751b949ac363a218c8f84c
TroyMoses <mugabimoses07@gmail.com>
2024-07-20 19:07:12 +0800

    home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,26 +13,65 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-                child: ListView(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 50, bottom: 20),
-                  child: const Text(
-                    'All ToDos',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                searchBox(),
+                Expanded(
+                    child: ListView(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 50, bottom: 20),
+                      child: const Text(
+                        'All ToDos',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    for (ToDo todo in todosList)
+                      ToDoItem(
+                        todo: todo,
+                      ),
+                  ],
+                )),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5,),
+                  decoration:
+                      const BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    ),],
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add new todo item',
+                      border: InputBorder.none
+                    ),
                   ),
                 ),
-                for (ToDo todo in todosList) ToDoItem(todo: todo,),
-              ],
-            )),
-          ],
-        ),
+              ),
+            ]),
+          )
+        ],
       ),
     );
   }
